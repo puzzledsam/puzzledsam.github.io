461c8dccba6be8637d3bc722d9b1bf4ea62ed89e
Sam Dion <samuelqwe@icloud.com>
2016-02-17 09:30:08 -0500

    Update main.js

--- healthplus-config/js/main.js
+++ healthplus-config/js/main.js
@@ -3,6 +3,6 @@ var $submitButton = $('submitButton');
 $submitButton.on('click', function() {
   console.log('Submit');
   
-  //var return_to = getQueryParam('return_to', 'pebblejs://close#');
-  /document.location = return_to + encodeURIComponent(JSON.stringify(getAndStoreConfigData()));
+  var return_to = getQueryParam('return_to', 'pebblejs://close#');
+  document.location = return_to + encodeURIComponent(JSON.stringify(getAndStoreConfigData()));
 });
